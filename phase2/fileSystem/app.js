5b109ac51101932c5529cdad728cc70ea5efb58d
sumit sharma <ssumit20kr@gmail.com>
2025-05-12 00:32:14 +0530

    write content in file FS

--- phase2/fileSystem/app.js
+++ phase2/fileSystem/app.js
@@ -1,8 +1,19 @@
 const fs=require('fs');
+//reade content of the file
 fs.readFile('message.txt', 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading file:', err);
     return;
   }
   console.log('File content:', data);
+});
+
+
+//write content in the file
+fs.writeFile('output.txt', 'This was written by Node.js!', (err) => {
+  if (err) {
+    console.error('Error writing file:', err);
+    return;
+  }
+  console.log('File written successfully!');
 });
