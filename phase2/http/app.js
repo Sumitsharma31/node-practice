75c59dc8e38d6de49f62a412865466f1ab14ee28
sumit sharma <ssumit20kr@gmail.com>
2025-05-15 23:41:39 +0530

    routing with node

--- phase2/http/app.js
+++ phase2/http/app.js
@@ -1,16 +1,24 @@
-const http=require('http');
+const http = require('http');
 
-const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  if (req.url==="/") {
+    res.setHeader('Content-Type', 'text/html');
+  res.write('<html>')
+  res.write('<head><title>Node</title></head>')
 
-    
-    res.setHeader('Content-Type','text/html');
+  res.write('<body><h1>Hello World!</h1></body>')
+  res.write('</html>')
+  return res.end();
+  }
+  else if (req.url === "/name") {
     res.write('<html>')
     res.write('<head><title>Node</title></head>')
 
-    res.write('<body><h1>Hello World!</h1></body>')
-    res.write('</html>')
-    res.end();
+    res.write('<body><h1>Hello Sumit!</h1></body>')
+    return res.write('</html>')
+
+  }
 });
 server.listen(3000, () => {
   console.log('Server is running at http://localhost:3000');
