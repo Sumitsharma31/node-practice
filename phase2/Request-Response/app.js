44af6c1a7802e96dc4d731a9631eaefbec2e143b
sumit sharma <ssumit20kr@gmail.com>
2025-05-16 21:16:39 +0530

    req-handle with node

--- phase2/Request-Response/app.js
+++ phase2/Request-Response/app.js
@@ -2,14 +2,16 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  if (req.url==="/") {
+ 
+  
+  if (req.url === "/") {
     res.setHeader('Content-Type', 'text/html');
-  res.write('<html>')
-  res.write('<head><title>Node</title></head>')
-
-  res.write('<body><h1>Hello World!</h1></body>')
-  res.write('</html>')
-  return res.end();
+    res.write('<html>')
+    res.write('<head><title>Node</title></head>')
+    
+    res.write('<body><h1>Hello World!</h1></body>')
+    res.write('</html>')
+    return res.end();
   }
   else if (req.url === "/name") {
     res.write('<html>')
