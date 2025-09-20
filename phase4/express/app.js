32dbe617736488a3b0a00d078e2ad736f254a6ce
sumit sharma <ssumit20kr@gmail.com>
2025-09-20 22:39:32 +0530

    learn middleware in express

--- phase4/express/app.js
+++ phase4/express/app.js
@@ -1,10 +1,34 @@
 const express = require("express");
-const app = express()
-
+const app = express();
 const PORT = 3001;
-app.listen(PORT, (res, req) => {
-  console.log(`server running on address http://localhost:${PORT}`);
 
+app.use((req, res, next) => {
+  console.log("First middleware:", req.url, req.method);
+  // res.send('<h1>Hello and welcome in first middleware</h1>');
+  next()
+});
+// app.use((req, res, next) => {
+//   console.log("First middleware:", req.url, req.method);
+//   res.send('<h1>Hello and welcome in second middleware</h1>');
+// });
+app.get("/contact-us", (req, res, next) => {
+  console.log("form-page", req.url, req.method);
+  res.send(`
+    <form action="/contact-us">
+          <input type="text" name="name" placeHolder="Name Please" />
+          <input type="text" name="email" placeHolder="Email Please" />
+          <input type="submit"  />
+    </form>
+    
+    `);
+  app.post("/contact-us", (req, res, next) => {
+    console.log("form-filled", req.url, req.method);
+    res.send(`Nice you did it`);
 
+  })
 
 })
+
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
