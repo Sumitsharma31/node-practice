8b6a849311e05c7ae77cb414950f9ee170f5e530
sumit sharma <ssumit20kr@gmail.com>
2026-02-05 22:38:19 +0530

    bcrypte encryption technique

--- Auth/app.js
+++ Auth/app.js
@@ -1,20 +1,42 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
+const bcrypt = require('bcrypt')
 
 const app = express();
 app.use(cookieParser())
+// cookies code
+// app.get('/', (req, res) => {
+
+//   res.cookie('name', "sumit");
+//   res.send("cookie saved")
+
+// })
+// app.get('/read', (req, res) => {
+//   console.log(req.cookies);
+//   res.send("sone")
+
+// })
+
+// Bcrypt start from here 
 
 app.get('/', (req, res) => {
 
-  res.cookie('name', "sumit");
-  res.send("cookie saved")
+  // Technique 1 (generate a salt and hash on separate function calls):
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash('password', salt, function (err, hash) {
+      console.log("method 1", hash);
+      res.send('done')
+    });
+  });
 
-})
-app.get('/read', (req, res) => {
-  console.log(req.cookies.name);
-  res.send("sone")
+  // Technique 2 (auto-gen a salt and hash):
+  bcrypt.hash('secret', 10, function (err, hash) {
+    console.log("method 2", hash);
 
+  });
 })
+
+
 app.listen(3000, () => {
   console.log("server started")
 
